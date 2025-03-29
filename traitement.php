9d08ed87d39fbfd1467431ec307c20e6bb6b1490
Labelive <remi.bl031@gmail.com>
2025-03-29 17:19:20 +0100

    modif php

--- traitement.php
+++ traitement.php
@@ -28,7 +28,7 @@ try {
     $mail->Username   = 'remi.bl031@gmail.com';                     //SMTP username
     $mail->Password   = 'ikfjjsdlwrnpiwdw';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = 587;   //465//                                 //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('from@example.com', 'Test');
